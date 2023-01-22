8817659632b7c26181fd8b2b0cedf1090f18d1fc
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-22 00:22:15 -0500

    data types

---
+++ data_types.dart
@@ -0,0 +1,17 @@
+/*
+Strongly Typed Language: The type of a variable is known at compile time.
+For example: C++, Java, Swift
+*/
+
+/*
+Dynamic Typed Language: The type of a variable is known at run time.
+For example: Python, Ruby, JavaScript.
+*/
+
+void main(List<String> args) {
+  int amount1 = 100;
+  var amount2 = 200;
+
+  print("Amount1: $amount1 | Amount2: $amount2");
+  
+}
